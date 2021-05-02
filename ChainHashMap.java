39da0e1718b457558d0771cbad327b31e4270912
Gabriel Cervo <joaogabrieldouradocervo@gmail.com>
2021-05-02 10:41:57 -0300

    Add get and put methods

--- ChainHashMap.java
+++ ChainHashMap.java
@@ -13,24 +13,63 @@ class Node {
 
 public class ChainHashMap extends AbstractMap {
     private ArrayList<Node> buckets;
+    private int size;
 
-    public ChainHashMap(int numberOfBuckets) {
-        buckets = new ArrayList<Node>(numberOfBuckets);
+    public ChainHashMap(int size) {
+        this.size = size;
+        initializeBuckets();
+    }
+
+    private void initializeBuckets() {
+        buckets = new ArrayList<Node>(this.size);
+        for (int i = 0; i < this.size; i++) {
+            buckets.add(null);
+        }
+    }
+
+    private int compress(long hash) {
+        return (int) (hash % this.size);
+    }
+
+    private int getIndex(String key) {
+        int hash = key.hashCode();
+        int index = compress(hash);
+
+        return index < 0 ? index * -1 : index;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public String get(String key) {
+        int index = getIndex(key);
+        Node head = buckets.get(index);
+        while (head != null) {
+            if (head.key.equals(key)) {
+                return head.value;
+            }
+            head = head.next;
+        }
 
         return null;
     }
 
     @Override
     public String put(String key, String value) {
+        int index = getIndex(key);
+        Node head = buckets.get(index);
+        while (head != null) {
+            if (head.key.equals(key)) {
+                String lastValue = head.value;
+                head.value = value;
+                return lastValue;
+            }
+            head = head.next;
+        }
+
         return null;
     }
 
