d1db8d7e3e42cb4f1df13df8663c3af55da086cd
Gabriel Cervo <joaogabrieldouradocervo@gmail.com>
2021-05-03 09:53:54 -0300

    New Testing

--- Main.java
+++ Main.java
@@ -1,19 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        ChainHashMap hashMap = new ChainHashMap(10000);
-        ChainHashMap hashMap2 = new ChainHashMap(100);
+        ChainHashMap chainHashMap = new ChainHashMap(10000);
+        ChainHashMap chainHashMap2 = new ChainHashMap(100);
 
         for (int i = 0; i < 1000000; i += 2) {
             String[] pares = { "a" + i, "" + (i + 1) };
-            hashMap.add(pares[0], pares[1]);
+            chainHashMap.add(pares[0], pares[1]);
         }
 
         for (int i = 0; i < 1000000; i += 2) {
             String[] pares = { "b" + i, "" + (i + 1) };
-            hashMap2.add(pares[0], pares[1]);
+            chainHashMap2.add(pares[0], pares[1]);
         }
 
-        System.out.println(hashMap.getLoadFactor());
-        System.out.println(hashMap2.getLoadFactor());
+        System.out.println(chainHashMap.getLoadFactor());
+        System.out.println(chainHashMap2.getLoadFactor());
+
+        System.out.println(chainHashMap.get("a999998"));
+        System.out.println(chainHashMap2.get("b999998"));
     }
 }
